b11a91e15c200f4357c47dda1b5c7af5a6d3cd75
Bilal <you@example.com>
2021-04-24 19:59:26 +0200

    Configuración de autenticación

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes(['register' => false]);
+
+Route::get('/home', 'HomeController@index')->name('home');
+
 Route::get('/{page}', 'AdminController@index');
+
+
