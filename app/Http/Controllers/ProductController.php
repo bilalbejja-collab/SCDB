99ecdcb8db5d1ae9813f3ea7621257e253afd112
Bilal Bejja <72463941+bilalbejja-collab@users.noreply.github.com>
2021-06-03 13:37:37 +0200

    proteger la ruta /products desde el controlador

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,6 +9,14 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     /**
+     * Only authenticated users can access.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
