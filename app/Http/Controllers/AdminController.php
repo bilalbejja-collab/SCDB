cecc0a79accacde19a16805a7f8fc2209ac3d37d
Bilal <you@example.com>
2021-06-10 01:35:38 +0200

    Añadir el middleware auth al AdminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,14 @@ namespace App\Http\Controllers;
 class AdminController extends Controller
 {
     /**
+     * Only authenticated users can access.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
