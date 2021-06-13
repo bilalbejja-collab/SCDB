10bb50de496b4a8706b9650b0f2da29733b240be
Bilal <you@example.com>
2021-06-13 23:39:03 +0200

    ordenadas las rutas

--- routes/web.php
+++ routes/web.php
@@ -28,49 +28,34 @@ Route::resource('sections', 'SectionController');
 Route::resource('products', 'ProductController');
 Route::resource('roles', 'RoleController');
 Route::resource('users', 'UserController');
-
 Route::resource('invoice-attachments', 'InvoicesAttachmentsController');
-
-Route::get('/section/{id}', 'InvoiceController@getProducts');
-
-Route::get('/invoices-details/{id}', 'InvoicesDetailsController@edit');
-
-Route::get('/download/{invoice_number}/{file_name}', 'InvoicesDetailsController@get_file');
-
-Route::get('/view-file/{invoice_number}/{file_name}', 'InvoicesDetailsController@open_file');
-
-Route::post('/delete-file', 'InvoicesDetailsController@destroy')->name('delete_file');
-
-Route::get('/edit-invoice/{id}', 'InvoiceController@edit');
-
-Route::get('/status-show/{id}', 'InvoiceController@show')->name('status-show');
-
-Route::post('/status-update/{id}', 'InvoiceController@statusUpdate')->name('status-update');
-
 Route::resource('archive', 'InvoicesArchiveController');
 
+Route::get('section/{id}', 'InvoiceController@getProducts');
+
+Route::get('status-show/{id}', 'InvoiceController@show')->name('status-show');
+Route::post('status-update/{id}', 'InvoiceController@statusUpdate')->name('status-update');
+
+Route::get('invoices-details/{id}', 'InvoicesDetailsController@edit');
+Route::get('edit-invoice/{id}', 'InvoiceController@edit');
 Route::get('paid-invoices', 'InvoiceController@paidInvoices');
-
 Route::get('unpaid-invoices', 'InvoiceController@unpaidInvoices');
-
 Route::get('partial-paid-invoices', 'InvoiceController@partialPaidInvoices');
-
 Route::get('print-invoice/{id}', 'InvoiceController@printInvoice');
-
-
 Route::get('export-invoices/', 'InvoiceController@export');
 
-Route::get('invoices-report', 'InvoicesReportController@index');
+Route::get('download/{invoice_number}/{file_name}', 'InvoicesDetailsController@get_file');
+Route::get('view-file/{invoice_number}/{file_name}', 'InvoicesDetailsController@open_file');
+Route::post('delete-file', 'InvoicesDetailsController@destroy')->name('delete_file');
 
+Route::get('invoices-report', 'InvoicesReportController@index');
 Route::post('search-invoices', 'InvoicesReportController@searchInvoices');
 
 Route::get('customers-report', 'CustomersReportController@index')->name("customers-report");
-
 Route::post('search-customers', 'CustomersReportController@searchCustomers');
 
 Route::get('mark-as-read-all', 'InvoiceController@markAsReadAll')->name('mark-as-read-all');
 
 Route::post('contact-us', 'UserController@contactUs');
-
 
 Route::get('/{page}', 'AdminController@index');
